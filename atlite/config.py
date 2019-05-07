dd668892ac74431ee02f55e9941f0e72075b5235
Leon <5868911+leonsn@users.noreply.github.com>
2019-05-07 15:35:08 +0200

    Adapt config to pepsimax

--- atlite/config.py
+++ atlite/config.py
@@ -1,7 +1,7 @@
 #Dataset Settings
-gebco_path = '/home/vres-climate/data/GEBCO_2014_2D.nc'
-cutout_dir = '/home/vres/data/cutouts'
-ncep_dir = '/home/vres-climate/data/rda_ucar'
+gebco_path = '/srv/data/REatlas/gebco_08.nc'
+cutout_dir = '/srv/data/atlite/cutouts'
+ncep_dir = '/srv/data/CFSR'
 cordex_dir = '/home/vres-climate/data/cordex/RCP8.5'
 sarah_dir = '/home/vres-climate/data/sarah_v2'
 weather_dataset = {'module': 'cordex', 'model': 'MPI-M-MPI-ESM-LR'}
