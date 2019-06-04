c79fdf0bca548761293cb128b254b600715f471a
Leon <5868911+leonsn@users.noreply.github.com>
2019-06-04 11:07:04 +0200

    Adapt config for PRIME

--- atlite/config.py
+++ atlite/config.py
@@ -1,7 +1,7 @@
 #Dataset Settings
-gebco_path = '/srv/data/REatlas/gebco_08.nc'
-cutout_dir = '/srv/data/atlite/cutouts'
-ncep_dir = '/srv/data/CFSR'
-cordex_dir = '/home/vres-climate/data/cordex/RCP8.5'
-sarah_dir = '/home/vres-climate/data/sarah_v2'
+gebco_path = '' #'/srv/data/REatlas/gebco_08.nc'
+cutout_dir = '/home/20115398/git_home/atlite/cutouts'
+ncep_dir = '' #'/srv/data/CFSR'
+cordex_dir = '' #'/home/vres-climate/data/cordex/RCP8.5'
+sarah_dir = '' #'/home/vres-climate/data/sarah_v2'
 weather_dataset = {'module': 'cordex', 'model': 'MPI-M-MPI-ESM-LR'}
